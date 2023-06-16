e35b36c0edf04f1706550d39952f752dd8ba73cb
Sakshi <sakshi.malviya013@gmail.com>
2023-06-16 14:15:35 +0530

    first commit

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require('express');
-const app= express();
+const mongoose = require('mongoose');
+
+const app = express();
 
 // route
-app.get('/',(req, res)=>{
+app.get('/', (req, res) => {
     res.send('hello node api');
+})
+
+app.get('/blog', (req, res) => {
+    res.send('hello blog my name is Sakshi                                                                                                                                                                                                                                                          ');
 })
 
 app.listen(3000, () => {
     console.log("hello api is runnig on port 3000");
+})
+
+mongoose.connect('mongodb+srv://admin:Number@123@apidatabase.d44uqfz.mongodb.net/Node-API?retryWrites=true&w=majority')
+.then(() => {
+ console.log('connected to MongoDB')
+}).catch((error)=>{
+    console.log('error')
 })
